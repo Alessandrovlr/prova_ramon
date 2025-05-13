bfa044bd4403e1bb29ffb3be03d51c523e7338c3
Alessandro <aleluinisilva@gmail.com>
2025-05-13 16:15:13 -0300

    criação do metodo get e correção no metodo post

--- api.js
+++ api.js
@@ -1,13 +1,34 @@
-import { registraLog } from "./script"
+const { registraLog } = require("./script")
 const express = require("express")
+const fs = require("fs")
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/logs/:id", (req, res) =>{
+app.get("/logs/:id", (req, res) => {
+    const { id } = req.params
 
-})
+    if(id.length !== 36){
+        return res.status(404).json( "Log não encontrado")
+
+    }else{
+        fs.readFile("logs.txt", "utf-8", (err, data) => {
+            if (err) {
+                return res.status(500).json("Erro ao ler o arquivo de logs" )
+            }
+    
+            const linhas = data.split("\n")
+            const logEncontrado = linhas.find((linha) => linha.startsWith(id))
+    
+            if (!logEncontrado) {
+                return res.status(404).json( "Log não encontrado")
+            }
+    
+            res.status(200).json( logEncontrado )
+        });
+    }
+});
 
 
 
@@ -20,7 +41,7 @@ app.post("/logs", async (req, res) => {
 
     try {
         const id = await registraLog(nome);
-        res.status(200).json(`Log registrado para ${nome}`, id);
+        res.status(200).json(`Log registrado com chave unica: ${id}`);
     } catch (err) {
         res.status(500).json("Erro ao registrar log" );
     }
