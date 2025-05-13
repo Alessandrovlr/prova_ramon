e2503595446c3ff0228be59fc6bed1b3ef797094
Alessandro <aleluinisilva@gmail.com>
2025-05-13 15:17:06 -0300

    criação dos metodos post e get da api

--- api.js
+++ api.js
@@ -1,5 +1,32 @@
+import { registraLog } from "./script"
 const express = require("express")
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
+
+app.get("/logs/:id", (req, res) =>{
+
+})
+
+
+
+app.post("/logs", async (req, res) => {
+    const { nome } = req.body;
+
+    if (!nome) {
+        return res.status(400).json( "Nome é obrigatório" );
+    }
+
+    try {
+        const id = await registraLog(nome);
+        res.status(200).json(`Log registrado para ${nome}`, id);
+    } catch (err) {
+        res.status(500).json("Erro ao registrar log" );
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('API rodando em http://localhost:3000');
+});
